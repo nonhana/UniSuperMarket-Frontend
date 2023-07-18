a3d145c052f9de6d20a4916146ba7e6dd5d71d98
Zhou Xiang <1209220829@qq.com>
2023-07-18 14:14:02 +0800

    初始化仓库。

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "vite";
+import uni from "@dcloudio/vite-plugin-uni";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import {resolve} from "path"
+// import Components from 'unplugin-vue-components/vite'
+// https://vitejs.dev/config/
+export default defineConfig({
+	
+	resolve:{
+		alias:[
+			{
+				find:"@",
+				replacement:resolve(__dirname,'src')
+			}
+		]
+	},
+	server:{
+		port:1314,
+		// 选项写法
+		proxy:{
+			'/pag': {
+				target: 'https://cdn.tmui.design',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, '/api')
+			},
+		}
+	},
+	plugins: [
+		uni(),
+		vueJsx()
+	]
+});
